c41668430aaba5d8984891ff356b3562b62a168c
FilipWirsen <Filip.wirsen99@gmail.com>
2022-05-09 10:49:57 +0000

    Remove old models and add SiteUser model and create signup form

--- restaurant/models.py
+++ restaurant/models.py
@@ -1,36 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 
-class Table(models.Model):
-    capacity = models.IntegerField()
-
-    def __str__(self):
-        return f"{self.capacity}"
-
-
-class Customer(models.Model):
-    first_name = models.CharField(max_length=25)
-    last_name = models.CharField(max_length=25)
-    slug = models.SlugField(max_length=25)
-    email = models.CharField(max_length=30, unique=True)
-
-    def __str__(self):
-        return f" {self.first_name} {self.last_name}"
-
-
-class Reservation(models.Model):
-    table_id = models.ForeignKey(Table, on_delete=models.CASCADE)
-    date = models.DateField()
-    time = models.CharField(max_length=5)
-    party_size = models.IntegerField()
-    customer_name = models.ForeignKey(Customer, on_delete=models.CASCADE)
-
-    class Meta:
-        ordering = ['-date', '-time']
-
-    def __str__(self):
-        return f"Date:{self.date}, Time:{self.time}, Table Number{self.table_id}"
-
+class SiteUser(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
 
--- restaurant/views.py
+++ restaurant/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from .forms import SignUpForm
+from django.contrib import messages
 
 # Create your views here.
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Account Created')
+            return render(request, 'index.html')
+    else:
+        form = SignUpForm()
+
+    args = {}
+    args['form'] = form
+    return render(request, 'signup.html', args)
+
+    # user = request.user
+    # profile = user.profile
+    #if we have a user that has already selected info, it will pass in this info
